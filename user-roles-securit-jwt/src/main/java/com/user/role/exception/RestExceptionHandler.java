fb26ac36cd739c6d403284f0b0759119c83f5664
narsimha-dev <snlshvsnj@gmial.com>
2020-02-26 16:11:23 +0530

    0.2: removed error files & rename token name:

---
+++ user-roles-securit-jwt/src/main/java/com/user/role/exception/RestExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.user.role.exception;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        String error=" ERROR JSON AGENT";
+        return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST,error,ex));
+    }
+
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return  new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+}
--- user-roles-securit-jwt/src/main/java/com/user/role/payload/response/JwtResponse.java
+++ user-roles-securit-jwt/src/main/java/com/user/role/payload/response/JwtResponse.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class JwtResponse {
 
 	private String token;
-	private String type = "Bearer";
+	private String token_type = "Bearer";
 	private Long id;
 	private String username;
 	private String email;
